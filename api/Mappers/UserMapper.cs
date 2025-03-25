8cf61a63ead788a03de2760c5335e9713e00e88d
Kevin Venegas <104701682+Kevin7819@users.noreply.github.com>
2025-03-25 13:09:27 -0600

    [2025.03.25 13.06h] Implementación de UserMapper (#4)

---
+++ api/Mappers/UserMapper.cs
@@ -0,0 +1,24 @@
+using api.Dtos.User;
+using api.Models;
+
+namespace api.Mappers
+{
+    public static class UserMapper
+    {
+        public static UserDto ToDto(this User )
+        {
+            return new UserDto
+            {
+
+            };
+        }
+
+        public static User ToFromCreateDto(this CreateUserRequestDto Dto)
+        {
+            return new User
+            {
+
+            };
+        }
+    }
+}
