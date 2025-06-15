b80b5829c0751562f81cb5bbffcb24a6651885ef
Kevin Venegas <104701682+Kevin7819@users.noreply.github.com>
2025-06-14 22:09:24 -0600

    [14/06/2025 21:22h]  Remove duplicate code from previous PR merge conflict (#8)

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -1,23 +1,17 @@
 
 using api.Models;
 
-using api.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 
 namespace api.Data
 {
     public class ApplicationDBContext : IdentityDbContext<User, IdentityRole<int>, int>
-    public class ApplicationDBContext : IdentityDbContext<User, IdentityRole<int>, int>
     {
-        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
 
         public DbSet<UserTask> UserTasks { get; set; }
-        public DbSet<Activity> Activities { get; set; }
         public DbSet<Activity> Activities { get; set; }
         public DbSet<Course> Courses { get; set; }
         public DbSet<PasswordResetCode> PasswordResetCodes { get; set; }
